5a123cf133f59aa5a3e651d594a2fec1162657d0
harshit duggal <135515880+Harshitduggal1@users.noreply.github.com>
2024-11-06 22:19:28 +0530

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,9 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { requireUser } from "@/app/lib/hooks";
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -12,23 +11,24 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
-
+      const session = await requireUser();
       // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
-
+      if (!session.user) throw new UploadThingError("Unauthorized");
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
-
       console.log("file url", file.url);
-
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
+
+
+//Using the requireUser function from @/app/lib/hooks to get the current user's session and extract the user ID.
+// pass the user's ID as the userId in the metadata object returned from the middleware function.
+//In the onUploadComplete function we can  now log the actual user ID who performed the upload.
